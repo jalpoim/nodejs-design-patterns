fd19bdd5f51799b5160bf9b9868622515a070434
Joao Alpoim <jchavesalp@gmail.com>
2024-05-04 16:53:51 +0100

    cps comments

--- examples/async-cps/index.js
+++ examples/async-cps/index.js
@@ -1,3 +1,8 @@
+/* The additionAsync function is asynchronous because setTimeout adds a task to the event queue that is 
+executed only after 100 milliseconds. This is an asynchronous operation, therefore the function will not wait 
+for the callback to complete its execution. Instead, it will return immediately giving control back to additionAsync 
+and then to the event loop, thus allowing a new event from the queue to be processed.
+ */
 function additionAsync(a, b, callback) {
   setTimeout(() => callback(a + b), 100);
 }
@@ -5,3 +10,8 @@ function additionAsync(a, b, callback) {
 console.log("before");
 additionAsync(1, 2, (result) => console.log("Result: " + result));
 console.log("after");
+
+// Output:
+// before
+// after
+// Result: 3
--- examples/sync-cps/index.js
+++ examples/sync-cps/index.js
@@ -4,6 +4,7 @@ function add(a, b) {
 }
 
 /* Continuation Passing Style */
+/* The addCps function is synchronous because it will complete it's execution only when the callback completes its execution too */
 function addCps(a, b, callback) {
   callback(a + b);
 }
@@ -11,3 +12,8 @@ function addCps(a, b, callback) {
 console.log("before");
 addCps(1, 2, (result) => console.log(`Result: ${result}`));
 console.log("after");
+
+// Output:
+// before
+// Result: 3
+// after
